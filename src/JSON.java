b36a26268c20c1daca15943cba188867b59bf91f
Chris Statkiewicz <r1ot.rog@gmail.com>
2015-04-21 10:45:15 +0200

    Comments added for all methods in use

--- src/JSON.java
+++ src/JSON.java
@@ -19,15 +19,21 @@ import org.json.simple.parser.ParseException;
 
 public class JSON {
 
+    //Variables regarding the JSON object
     private long[] values;
     private String[] names;
     private String whichFuncReq = "";
 
-
+    //Variable sregarding the JSON file
     protected String path;
     protected String fileNa;
     private String fileExtension = ".txt";
 
+    /**
+     * COnstructor using a path/name of file to create a JSON object
+     * @param filePath String
+     * @param fileName String
+     */
     public JSON(String filePath, String fileName){
             //Open JSON
         this.path = filePath;
@@ -58,7 +64,12 @@ public class JSON {
 
     }
 
-
+    /**
+     * Constructor using two arrays and an integer to create a JSON object
+     * @param arr long [] containing data series
+     * @param nam String [] containing data names/labels
+     * @param fr  int setting the functional req. to be used
+     */
     public JSON (long []arr, String[] nam, int fr) {
         this.values = arr;
         this.names = nam;
@@ -72,6 +83,9 @@ public class JSON {
         }
     }
 
+    /**
+     * Method which creates a .json file given a JSON object instance
+     */
     public void outputFile(){
         JSONObject jsonObject = new JSONObject();
 
@@ -109,14 +123,34 @@ public class JSON {
 
     }
 
+    /**
+     * Getter of array of data series
+     * @return values
+     */
     public long[] getValues(){ return this.values; }
 
+    /**
+     * Getter of path String
+     * @return path
+     */
     public String getPath() { return path; }
 
+    /**
+     * Getter of file name String
+     * @return fileNa
+     */
     public String getFileNa() { return fileNa; }
 
+    /**
+     * Setter for path String
+     * @param path String
+     */
     public void setPath(String path) { this.path = path; }
 
+    /**
+     * Setter for file name String
+     * @param fileNa String
+     */
     public void setFileNa(String fileNa) { this.fileNa = fileNa; }
 
 }
--- src/JSONStatsWrapper.java
+++ src/JSONStatsWrapper.java
@@ -14,14 +14,22 @@ public class JSONStatsWrapper {
         json = in;
     }
 
-    /* Computes the summatory of one or several data arrays (subtracted at each step) with optional power computing at each step*/
-    public long Sumatory(int exp, int typeOfSumatory, long[] optional) {
+    /**
+     * Computes the summation of one or several data arrays
+     * (subtracted at each step) with optional power computing at each step
+     * @param exp int with exponent of each summation step
+     * @param typeOfSummation
+     * values: 0 = no exp, 1 = 1/xi no exp, 2 = xi-mean(x) with exp, 3 = (Xi - /x)(Yi - /y)
+     * @param optional second long array, used in type 3.
+     * @return result of summation (long)
+     */
+    public long Summation(int exp, int typeOfSummation, long[] optional) {
         long sum = 0;
         long[] a = json.getValues();
 
-        //This switch will compute the different types of sumatory.
-        switch (typeOfSumatory) {
-            //Normal sumatory with no exp
+        //This switch will compute the different types of summation.
+        switch (typeOfSummation) {
+            //Normal summation with no exp
             case 0:
                 //Retrieve array of values
                 sum = 0;
@@ -30,7 +38,7 @@ public class JSONStatsWrapper {
                 }
                 break;
 
-            //Sumatory of 1/xi with no exp
+            //summation of 1/xi with no exp
             case 1:
                 sum = 0;
                 for (int n = 0; n < a.length; n++) {
@@ -38,12 +46,12 @@ public class JSONStatsWrapper {
                 }
                 break;
 
-            //SUmatory of xi - /x with exp
+            //summation of xi - /x with exp
             //Not developed, due to unnecesariness
             case 2:
                 break;
 
-            //Symatory of (Xi - /x)(Yi - /y)
+            //summation of (Xi - /x)(Yi - /y)
             //Not developed, due to unnecesariness
             case 3:
                 break;
@@ -53,12 +61,12 @@ public class JSONStatsWrapper {
 
     }
 
-
-    /*Compute quartiles, and percentiles if possible*/
-
-    /*In the srs example given, it says calculate nearest, but, if it's the lower bound, it can be actually lower than
-    *the real n-th percentile/quartile, since it leaves behind less than n%
-    */
+    /**
+     * Compute quartiles, and percentiles if possible
+     * In the srs example given, it says calculate nearest, but, if it's the lower bound, it can be actually lower than
+     * the real n-th percentile/quartile, since it leaves behind less than n%
+     * @throws IOException
+     */
     public void verification() throws IOException {
 
         //We start by retrieving the array from the JSON
@@ -82,7 +90,10 @@ public class JSONStatsWrapper {
     }
 
 
-    /*Computation of mean, median, mode, geometric_mean, harmonic_mean*/
+    /**
+     * Computation of mean, median, mode, geometric_mean, harmonic_mean
+     * @throws IOException
+     */
     public void predictionInterval() throws IOException {
 
         long[] a = json.getValues();
@@ -104,6 +115,10 @@ public class JSONStatsWrapper {
         }
     }
 
+    /**
+     * Writes a log whenever bounds for a computation are not fulfilled
+     * @throws IOException
+     */
     public void errorOutput() throws IOException {
         Date date = new Date();
         String content = "If numbers are shown, insufficient amount of values, if not, too large amount (min: 30, max = 10⁸)" + "\n";
